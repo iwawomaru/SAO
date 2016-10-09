d8c0f0b48f4905af779b474cd3e7533cf8762871
YutaAsupara <y.ashi@xcompass.com>
2016-10-10 05:47:20 +0900

    add dqn.py

--- noh/components/dqn.py
+++ noh/components/dqn.py
@@ -10,7 +10,8 @@ from keras import backend as K
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
-from keras.layers import Input, Dense, Reshape
+from keras.layers import Input, Dense, Reshape, ZeroPadding2D
+from keras.layers.pooling import MaxPooling2D
 from keras.layers.wrappers import TimeDistributed
 from keras.optimizers import Adam, Adamax, RMSprop
 from keras.layers.advanced_activations import PReLU
@@ -23,7 +24,6 @@ import memory
 input_dim = 80 * 80
 gamma = 0.99
 update_frequency = 1
-learning_rate = 0.001
 resume = False
 render = False
 INITIAL_EPSILON = 1
@@ -33,10 +33,12 @@ explorationRate = INITIAL_EPSILON
 stepCounter = 0
 current_epoch = 0
 loadsim_seconds = 0
+minibatch_size = 1
 
 class DQN:
+
     
-    def __init__(self, n_output, memorySize=100000, discountFactor=0.95, learningRate=1e-3, learnStart=10000):
+    def __init__(self, n_output, memorySize=100000, discountFactor=0.95, learningRate=1e-3, learnStart=1000):
         """
         Parameters:
         - outputs : output size
@@ -49,31 +51,50 @@ class DQN:
         self.discountFactor = discountFactor
         self.learningRate = learningRate
         self.learnStart = learnStart
+        self.explorationRate = INITIAL_EPSILON
 
     def __call__(self,state):
         qvalues = self.getQValues(state)
-        return self.selectAction(qvalues, explorationRate)
+        return self.selectAction(qvalues, self.explorationRate)
 
     def initNetworks(self):
         model = self.createModel()
         self.model = model
     
-    def createModel(self,input_dim=80*80, model_type=0):
+    def createModel(self,input_dim=80*80, model_type=1):
         model = Sequential()
         if model_type==0:
             model.add(Reshape((1,80,80), input_shape=(input_dim,)))
             model.add(Flatten())
             model.add(Dense(200, activation = 'relu'))
             model.add(Dense(self.output_size, activation='softmax'))
-            opt = RMSprop(lr=learning_rate)
+            opt = RMSprop(lr=self.learningRate)
         else:
             model.add(Reshape((1,80,80), input_shape=(input_dim,)))
+            '''
+            model.add(Convolution2D(16, 3, 3, subsample=(2,2)))
+            model.add(Activation('relu'))
+            model.add(ZeroPadding2D((1, 1)))
+            model.add(Convolution2D(16, 3, 3, subsample=(2,2)))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+            model.add(Flatten())
+            model.add(Dense(256))
+            model.add(Activation('relu'))
+            model.add(Dense(network_outputs))
+            adam = Adam(lr=self.learningRate)
+            model.compile(loss='mse',optimizer=adam)
+            #model.compile(RMSprop(lr=self.learningRate), 'MSE')
+            model.summary()
+            '''
             model.add(Convolution2D(32, 9, 9, subsample=(4, 4), border_mode='same', activation='relu', init='he_uniform'))
             model.add(Flatten())
             model.add(Dense(16, activation='relu', init='he_uniform'))
             model.add(Dense(self.output_size, activation='softmax'))
-            opt = Adam(lr=learning_rate)
-        model.compile(loss='categorical_crossentropy', optimizer=opt)
+            opt = Adam(lr=self.learningRate)
+            
+            model.compile(loss='categorical_crossentropy', optimizer=opt)
+            
         return model
     
     def printNetwork(self):
@@ -99,7 +120,7 @@ class DQN:
 
     # predict Q values for all the actions
     def getQValues(self, state):
-        print state.shape
+        #print state.shape
         predicted = self.model.predict(state.reshape([1,state.shape[0]]),batch_size=1)
         return predicted[0]
 
@@ -157,6 +178,8 @@ class DQN:
             if (rand <= value):
                 return i
             i += 1
+    def set_reward(self, reward):
+        self.reward = reward
 
     def addMemory(self, state, action, reward, newState, isFinal):
         self.memory.addMemory(state, action, reward, newState, isFinal)
@@ -166,12 +189,15 @@ class DQN:
             return self.memory.getMemory(self.memory.getCurrentSize() - 1)
 
     def learnOnMiniBatch(self, miniBatchSize, useTargetNetwork=True):
+        # print(miniBatchSize)
         # Do not learn until we've got self.learnStart samples        
         if self.memory.getCurrentSize() > self.learnStart:
             # learn in batches of 128
             miniBatch = self.memory.getMiniBatch(miniBatchSize)
-            X_batch = np.empty((1,img_channels,img_rows,img_cols), dtype = np.float64)
+            '''
+            X_batch = np.empty((6400,), dtype = np.float64)
             Y_batch = np.empty((1,self.output_size), dtype = np.float64)
+            '''
             for sample in miniBatch:
                 isFinal = sample['isFinal']
                 state = sample['state']
@@ -185,14 +211,29 @@ class DQN:
                 else :
                     qValuesNewState = self.getQValues(newState)
                 targetValue = self.calculateTarget(qValuesNewState, reward, isFinal)
+                #print(state.copy().shape)
+                '''
                 X_batch = np.append(X_batch, state.copy(), axis=0)
+                print(X_batch.shape)
                 Y_sample = qValues.copy()
                 Y_sample[action] = targetValue
                 Y_batch = np.append(Y_batch, np.array([Y_sample]), axis=0)
+                print(Y_batch.shape)
+                '''
+                X_batch =  state.copy()
+                #print(X_batch.shape)
+                Y_sample = qValues.copy()
+                Y_sample[action] = targetValue
+                Y_batch = np.array([Y_sample])
+                #print(Y_batch.shape)
+                '''
                 if isFinal:
                     X_batch = np.append(X_batch, newState.copy(), axis=0)
                     Y_batch = np.append(Y_batch, np.array([[reward]*self.output_size]), axis=0)
-            self.model.fit(X_batch, Y_batch, validation_split=0.2, batch_size = len(miniBatch), nb_epoch=1, verbose = 0)
+                '''
+                #X_batch = X_batch.reshape([1,X_batch[0]])
+                #print len(miniBatch)
+            self.model.fit(X_batch.reshape([1,X_batch.shape[0]]), Y_batch, validation_split=0.2, batch_size = len(miniBatch), nb_epoch=1, verbose = 0)
 
     def saveModel(self, path):
         self.model.save(path)
@@ -200,8 +241,25 @@ class DQN:
     def loadWeights(self, path):
         self.model.set_weights(load_model(path).get_weights())
     
-    def reinforcement_train(self): pass
+    def reinforcement_train(self): 
+        self.addMemory(self.state,self.action, self.reward, self.observation, self.done) 
+        stepCounter = self.frame
+        if self.explorationRate > FINAL_EPSILON and stepCounter > self.learnStart:
+            self.explorationRate -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
+        
+        if stepCounter == self.learnStart:
+            print("Starting learning")
 
+        if stepCounter >= self.learnStart:
+            self.learnOnMiniBatch(minibatch_size, False)
+        
+    def get_info(self, state, action, reward, observation, done, frame):
+        self.state = state
+        self.action = action
+        self.reward = reward
+        self.observation = observation
+        self.done = done
+        self.frame = frame
 
 def detect_monitor_files(training_dir):
     return [os.path.join(training_dir, f) for f in os.listdir(training_dir) if f.startswith('openaigym')]
